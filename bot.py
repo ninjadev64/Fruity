6e75f7c673c9add564729d8329d8f0efad9513e5
ninjadev64 <aman@amansprojects.com>
2021-10-06 19:27:28 +0100

    Make embed declarations a bit cleaner, fix mention reaction, make maths easier, only mark answers if they're in the same channel they were requested in, add first badge (badges will be implemented later)

--- bot.py
+++ bot.py
@@ -18,7 +18,8 @@ with sqlite3.connect("fruity.db") as db:
     cursor=db.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS Answers(
     ID text PRIMARY KEY,
-    Answer text NOT NULL);""")
+    Answer text NOT NULL,
+    Channel text NOT NULL);""")
 cursor.execute("""CREATE TABLE IF NOT EXISTS Points(
     ID text PRIMARY KEY,
     Points integer NOT NULL);""")
@@ -62,7 +63,7 @@ async def on_dropdown(ctx):
 class HelpEmbeds:
     @staticmethod
     def getFunEmbed():
-        embed=deepcopy(template_help_embed)
+        embed = deepcopy(template_help_embed)
         embed.add_field(name="/animal [animal]", value="Get an animal fact and cute image", inline=False)
         embed.add_field(name="/joke", value="Random joke generator", inline=False)
         embed.add_field(name="/echo [input]", value="Echo your input", inline=False)
@@ -70,7 +71,7 @@ class HelpEmbeds:
 
     @staticmethod
     def getMinigamesEmbed():
-        embed=deepcopy(template_help_embed)
+        embed = deepcopy(template_help_embed)
         embed.add_field(name="/math", value="Do a short maths equation", inline=False)
         embed.add_field(name="/unscramble", value="Unscramble a jumbled-up word", inline=False)
         embed.add_field(name="/coinflip [side]", value="Flip a coin", inline=False)
@@ -78,14 +79,14 @@ class HelpEmbeds:
 
     @staticmethod
     def getPointsEmbed():
-        embed=deepcopy(template_help_embed)
+        embed = deepcopy(template_help_embed)
         embed.add_field(name="/points [user]", value="See how many points a user has", inline=False)
         embed.add_field(name="/leaderboard", value="View the top 5 players for points", inline=False)
         return embed
 
     @staticmethod
     def getOtherEmbed():
-        embed=deepcopy(template_help_embed)
+        embed = deepcopy(template_help_embed)
         embed.add_field(name="/credits", value="The people behind the bot", inline=False)
         embed.add_field(name="/invite", value="Invite the bot to your server", inline=False)
         embed.add_field(name="/ping", value="Ping? Pong!", inline=False)
@@ -147,17 +148,16 @@ async def help(ctx, category="fun"):
 async def math(ctx):
     num1 = random.randint(1,50)
     num2 = random.randint(1,50)
-    operation = random.randint(0,2)
-    operation_strings = [" + ", " - ", " x "]
+    operation = random.randint(0, 1)
+    operation_strings = [" + ", " - "]
 
-    embed=deepcopy(template_embed)
+    embed = deepcopy(template_embed)
     embed.add_field(name="Solve this", value=str(num1) + operation_strings[operation] + str(num2), inline=True)
     await ctx.send(embed=embed)
     if operation == 0: answer = num1 + num2
     if operation == 1: answer = num1 - num2
-    if operation == 2: answer = num1 * num2
-    cursor.execute("""INSERT OR REPLACE INTO Answers(ID, Answer)
-    VALUES(?,?)""", (ctx.author.id, answer))
+    cursor.execute("""INSERT OR REPLACE INTO Answers(ID, Answer, Channel)
+    VALUES(?,?,?)""", (ctx.author.id, answer, ctx.channel.id))
     db.commit()
 
 @slash.slash_command(description="Unscramble a jumbled-up word")
@@ -169,18 +169,18 @@ async def unscramble(ctx):
     if word_scrambled == word:
         await unscramble(ctx)
         return
-    embed=deepcopy(template_embed)
+    embed = deepcopy(template_embed)
     embed.add_field(name="Unscramble this", value=word_scrambled, inline=True)
     await ctx.send(embed=embed)
-    cursor.execute("""INSERT OR REPLACE INTO Answers(ID, Answer)
-    VALUES(?,?)""", (ctx.author.id, word))
+    cursor.execute("""INSERT OR REPLACE INTO Answers(ID, Answer, Channel)
+    VALUES(?,?,?)""", (ctx.author.id, word, ctx.channel.id))
     db.commit()
 
 @slash.slash_command(description="Flip a coin", options=[
         Option("side", "Side", OptionType.STRING, True, [OptionChoice("heads", "heads"), OptionChoice("tails", "tails")])
 ])
 async def coinflip(ctx, side=None):
-    embed=deepcopy(template_embed)
+    embed = deepcopy(template_embed)
     flipped_side = random.choice(["heads", "tails"])
     if flipped_side == side:
         embed.colour = discord.Color.green()
@@ -198,10 +198,10 @@ async def coinflip(ctx, side=None):
 async def points(ctx, user=None):
     user = user or ctx.author
     cursor.execute("SELECT Points FROM Points WHERE ID = ?", (user.id,))
-    points_data=cursor.fetchone()
+    points_data = cursor.fetchone()
     if points_data is None: cursor.execute("""INSERT INTO Points(ID, Points)
     VALUES(?,?)""", (user.id, 0))
-    embed=deepcopy(template_embed)
+    embed = deepcopy(template_embed)
     cursor.execute("SELECT Points from Points WHERE ID = ?", (user.id,))
     for x in cursor.fetchall():
         embed.add_field(name="Points", value=user.name + "#" + user.discriminator + " has " + str(x[0]) + " points.", inline=False)
@@ -210,11 +210,13 @@ async def points(ctx, user=None):
 @slash.slash_command(description="View the top 5 players for points")
 async def leaderboard(ctx):
     strings = []
-    embed=deepcopy(template_embed)
+    embed = deepcopy(template_embed)
     cursor.execute("SELECT * FROM Points ORDER BY Points DESC LIMIT 5")
+    place = 1
     for x in cursor.fetchall():
         user = await bot.fetch_user(x[0])
-        strings.append(user.name + "#" + user.discriminator + ": " + str(x[1]))
+        strings.append(str(place)+". " + user.name + "#" + user.discriminator + ": " + str(x[1]))
+        place = place + 1
     embed.add_field(name="Leaderboard", value="\n".join(strings), inline=False)
     await ctx.send(embed=embed)
 
@@ -222,7 +224,7 @@ async def leaderboard(ctx):
         Option("animal", "Animal", OptionType.STRING, True, [OptionChoice("dog", "dog"), OptionChoice("cat", "cat"), OptionChoice("panda", "panda"), OptionChoice("fox", "fox"), OptionChoice("koala", "koala"), OptionChoice("bird", "bird")])
 ])
 async def animal(ctx, animal=None):
-    embed=deepcopy(template_embed)
+    embed = deepcopy(template_embed)
     embed.add_field(name=animal.capitalize(), value=requests.get(sra + "facts/" + animal).json().get("fact"))
     embed.set_image(url=requests.get(sra + "img/" + animal).json().get("link"))
     embed.set_footer(text="Powered by Some Random API", icon_url="https://i.some-random-api.ml/logo.png")
@@ -230,7 +232,7 @@ async def animal(ctx, animal=None):
 
 @slash.slash_command(description="Random joke generator")
 async def joke(ctx):
-    embed=deepcopy(template_embed)
+    embed = deepcopy(template_embed)
     json = requests.get("https://v2.jokeapi.dev/joke/Any?safe-mode").json()
     if json.get("type") == "single": embed.add_field(name="Joke", value=json.get("joke"), inline=False)
     if json.get("type") == "twopart":
@@ -244,7 +246,7 @@ async def joke(ctx):
 ])
 async def lyrics(ctx, song=""):
     json = requests.get(sra + "lyrics?title=" + song).json()
-    embed=deepcopy(template_embed)
+    embed = deepcopy(template_embed)
     embed.set_footer(text="Powered by Some Random API", icon_url="https://i.some-random-api.ml/logo.png")
     if json.get("error") is not None:
         embed.add_field(name="Error", value=json.get("error"), inline=False)
@@ -271,25 +273,28 @@ async def on_message(message):
     
     # React to message if it mentions the bot
     if bot.user.mentioned_in(message) and message.channel.guild.me.guild_permissions.add_reactions:
-        await message.add_reaction(get(bot.get_guild(874266744456376370).emojis, name='Fruity Mention Reaction'))
+        await message.add_reaction(get(bot.get_guild(874266744456376370).emojis, name='FruityMentionReaction'))
 
     # Handle answer marking
     cursor.execute("SELECT Answer FROM Answers WHERE ID = ?", (message.author.id,))
-    data=cursor.fetchone()
-    if data is not None:
-        embed=deepcopy(template_embed)
+    stored_answer = cursor.fetchone()
+    cursor.execute("SELECT Channel FROM Answers WHERE ID = ?", (message.author.id,))
+    channel_id = cursor.fetchone()
+    if stored_answer is not None:
+        if not channel_id[0] == str(message.channel.id): return
+        embed = deepcopy(template_embed)
         cursor.execute("SELECT Points FROM Points WHERE ID = ?", (message.author.id,))
-        points_data=cursor.fetchone()
-        if points_data is None: cursor.execute("""INSERT INTO Points(ID, Points)
+        stored_points = cursor.fetchone()
+        if stored_points is None: cursor.execute("""INSERT INTO Points(ID, Points)
         VALUES(?,?)""", (message.author.id, 0))
-        if msg == data[0]:
+        if msg == stored_answer[0]:
             embed.colour = discord.Color.green()
-            embed.add_field(name="Correct!", value=data[0] + " was the correct answer!", inline=False)
+            embed.add_field(name="Correct!", value=stored_answer[0] + " was the correct answer!", inline=False)
             embed.set_footer(text="(+5 points)")
             cursor.execute("UPDATE Points SET Points = Points + 5 WHERE ID = ?", (message.author.id,))
         else:
             embed.colour = discord.Color.red()
-            embed.add_field(name="Incorrect!", value=data[0] + " was the correct answer!", inline=False)
+            embed.add_field(name="Incorrect!", value=stored_answer[0] + " was the correct answer!", inline=False)
             embed.set_footer(text="(-2 points)")
             cursor.execute("UPDATE Points SET Points = Points - 2 WHERE ID = ?", (message.author.id,))
         await message.reply(embed=embed, mention_author=False)
@@ -304,20 +309,20 @@ async def on_message(message):
 
 @slash.slash_command(description="The people behind the bot")
 async def credits(ctx):
-    embed=deepcopy(template_embed)
+    embed = deepcopy(template_embed)
     embed.add_field(name="Developer(s)", value="ninjagamer64#0861 (aka ninjadev64)", inline=False)
     embed.add_field(name="Random stuff and ideas (unofficial)", value="Blaze#2299\nPerestuken#8688 / Perestuken#6263", inline=False)
     await ctx.send(embed=embed)
 
 @slash.slash_command(description="Invite the bot to your server")
 async def invite(ctx):
-    embed=deepcopy(template_embed)
+    embed = deepcopy(template_embed)
     embed.add_field(name="Invite the bot to your server", value="Please note that while the bot is in development you won't be able to use slash commands in your server!\nhttps://ninjadev64.github.io/Fruity/", inline=False)
     await ctx.send(embed=embed)
 
 @slash.slash_command(description="Ping? Pong!")
 async def ping(ctx):
-    embed=deepcopy(template_embed)
+    embed = deepcopy(template_embed)
     embed.add_field(name="Ping? Pong!", value=str(round(bot.latency * 1000)) + "ms", inline=False)
     await ctx.send(embed=embed)
 
@@ -347,7 +352,7 @@ async def prefixed_ping(ctx): await ping(ctx)
         Option("suggestion", "Suggestion", OptionType.STRING, True)
 ])
 async def suggest(ctx, project=None, suggestion=None):
-    embed=deepcopy(template_embed)
+    embed = deepcopy(template_embed)
     embed.set_author(name="Suggestion")
     embed.add_field(name=project, value=suggestion, inline=False)
     embed.set_footer(text=ctx.author.name + "#" + ctx.author.discriminator, icon_url=ctx.author.avatar_url)
